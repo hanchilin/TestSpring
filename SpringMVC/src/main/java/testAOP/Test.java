6e524a0c8ed2347085794ea0314ad47b91edd12b
hanchilin <cw_jdmc@126.com>
2015-03-31 17:17:02 +0800

    test aop

--- SpringMVC/src/main/java/testAOP/Test.java
+++ SpringMVC/src/main/java/testAOP/Test.java
@@ -3,20 +3,18 @@ package testAOP;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import testInterface.MindReader;
+import com.hanchilin.SpringMVC.Performer;
+
 
 public class Test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		/*ApplicationContext ac = new ClassPathXmlApplicationContext("ApplicationContext.xml");
 		MindReader mr = (MindReader) ac.getBean("magcian");
 		mr.interceptThoughts("test");*/
 		
 		ApplicationContext ac = new ClassPathXmlApplicationContext("ApplicationContext.xml");
-		Audience ad = (Audience) ac.getBean("audience");
-		ad.takeSeats();
-		ad.turnOffCellPhone();
-		ad.applaud();
-		ad.demandRefund();
+		Performer pf = (Performer) ac.getBean("duke");
+		pf.perform();
 	}
 }
